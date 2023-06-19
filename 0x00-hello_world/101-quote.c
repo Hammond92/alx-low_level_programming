85ad4db10de54daa429b22100d21203928f243c4
Hammond92 <isaacappohhammond@gmail.com>
2023-06-19 09:40:49 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main - line that prints new line
+ *Retun: Always 1.
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-190\n", stdout);
+	return (1);
+}
