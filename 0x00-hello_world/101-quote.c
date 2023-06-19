ae1044239930df1777dcf93f8b46e96c2dc8805a
Hammond92 <isaacappohhammond@gmail.com>
2023-06-19 09:44:50 -0700

    100-intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
- *main - line that prints new line
+ *main - "line that prints new line"
  *Retun: Always 1.
  */
 
 int main(void)
+
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-190\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
