6749a31e4f08515926d8aa349a8c2281159e0ca9
Hammond92 <isaacappohhammond@gmail.com>
2023-06-19 21:08:36 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digit numbers 
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char a;
+
+	a = 'z';
+	while
+		(a >= 'a')
+		{
+			putchar(a);
+			a--;
+		}
+	putchar('\n');
+	
+	return (0);
+}
