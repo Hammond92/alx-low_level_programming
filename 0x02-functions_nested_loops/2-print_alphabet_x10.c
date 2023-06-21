c8ed4e37933c92ce59a94007a636d307b68ea162
Hammond92 <isaacappohhammond@gmail.com>
2023-06-20 19:02:36 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,58 +7,20 @@
  */
 
 void print_alphabet_x10(void)
+
 {
-        char letter;
-
-        for (letter = 'a'; letter <= 'z'; letter++)
-                _putchar(letter);
-
-        _putchar('\n');
-
+	char letter;
+	int i;
+	i = 0;
+	while (i < 10)
+	{
 	for (letter = 'a'; letter <= 'z'; letter++)
-                _putchar(letter);
+	{
+         _putchar(letter);
+	}
 
-        _putchar('\n');
+	_putchar('\n');
+	letter++;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-                _putchar(letter);
-
-        _putchar('\n');
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-                _putchar(letter);
-
-        _putchar('\n');
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-                _putchar(letter);
-
-        _putchar('\n');
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-                _putchar(letter);
-
-        _putchar('\n');
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-                _putchar(letter);
-
-        _putchar('\n');
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-                _putchar(letter);
-
-        _putchar('\n');
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-                _putchar(letter);
-
-        _putchar('\n');
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-                _putchar(letter);
-
-        _putchar('\n');
-
-        return (0);
+	return (0);
 }
