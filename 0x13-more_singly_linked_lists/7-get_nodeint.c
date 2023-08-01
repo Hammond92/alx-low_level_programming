70078cbed3aa1f04e024f1e85d02c55239f80c2b
Hammond92 <isaacappohhammond@gmail.com>
2023-08-01 16:26:53 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -6,7 +6,7 @@
  * of a listint_t linked list.
  * @head: first node
  * @index: index to return
- * Return: if the node does not exist, return NULL
+ * Return: if te node does not exist, return NULL
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,9 +16,10 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 {
+	unsigned int i;
 	listint_t *new = malloc(sizeof(listint_t));
 	listint_t *temp = *head;
-	unsigned int i;
+	
 	if (!new || !head)
 		return (NULL);
 	
