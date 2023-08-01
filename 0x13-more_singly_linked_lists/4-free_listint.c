81d784fd44cbe5ff294ded84e3ae690e693356e0
Hammond92 <isaacappohhammond@gmail.com>
2023-08-01 15:30:30 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * free_listint -  a function that frees a listint_t list
+ * @head: pointer to listint_t
+ */
+
+void free_listint(listint_t *head)
+
+{
+	listint_t *temp;
+
+		if (head == NULL)
+			return;
+	while(*head)
+	{
+		temp = (*head)->next; free(*head); *head = temp;
+	}
+	*head = NULL;
+}
+
