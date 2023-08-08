63a10b12d7a3373233e79ea05594d99b79647475
Hammond92 <isaacappohhammond@gmail.com>
2023-08-07 21:53:43 -0700

    2-append_text_to_file.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <sts/types.>
+#include <sys/types.h>
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
