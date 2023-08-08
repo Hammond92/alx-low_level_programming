6420264d57b67d21c50adf2f78d9baf562c55ab8
Hammond92 <isaacappohhammond@gmail.com>
2023-08-07 21:44:28 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  * Return: if text_content is NULL create an empty file
  */
 
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 
 {
 	int fd, w, len = 0;
