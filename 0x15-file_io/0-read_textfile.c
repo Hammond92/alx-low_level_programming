3d756e3c2e1b8997edc96ca761b79e8bc6688d2c
Hammond92 <isaacappohhammond@gmail.com>
2023-08-07 22:10:40 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 
 /**
@@ -23,8 +24,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	return (0);
 
 	buf = malloc(sizeof(char) *letters); t = read(fd, buf, letters);
-	w = write(STDOUT_FILEND, buf, t); free(buf);
+	w = write(STDOUT_FILENO, buf, t); free(buf);
 
-	clse(fd);
+	close(fd);
 	return (0);
 }
